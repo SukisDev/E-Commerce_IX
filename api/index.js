a3cda884319c567a93f2d10d20126a4658d1a29c
SukisDev <jeang0125@gmail.com>
2025-02-16 21:48:11 -0500

    Avances Moongoose

--- api/index.js
+++ api/index.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const products = require("./data/products")
 dotenv.config();
+const PORT = process.env.PORT;
 
+const moogoose = require("mongoose");
+const { default: mongoose } = require("mongoose");
 
-// test route
-app.get("/api/products", (req, res) => {
-    const products = products;
-    res.json(products)
+// connect db
+mongoose.connect(process.env.MOONGOOSEDB_RUL).then(()=>console.log("db Connected")).then((err)=>{
+    err;
 })
 
-const PORT = process.env.PORT;
+
 app.listen(PORT || 9000, ()=> {
     console.log(`Server Listening on Port ${PORT}`)
 })
+
+
+// jeang0125
+// NylKWSDnDEmCK81h
+// mongodb+srv://jeang0125:NylKWSDnDEmCK81h@cluster0.xaqs2.mongodb.net/React-node-app
+
+
+
+/*
+// Api product test route
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((product)=>product.id == req.params.id)
+    res.json(product)
+})
+*/
